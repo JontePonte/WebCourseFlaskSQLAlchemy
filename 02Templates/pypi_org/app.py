ac8200fe1545792282d339ddd5f0b0d3cc63a789
JontePonte <john_nordstrand@hotmail.com>
2020-11-17 08:18:44 +0100

    added second part

---
+++ 02Templates/pypi_org/app.py
@@ -0,0 +1,28 @@
+
+import flask
+
+app = flask.Flask(__name__)
+
+
+def get_latest_packages():
+    return [
+        {'name': 'flask', 'version': '1.2.3'},
+        {'name': 'sqlalchemy', 'version': '2.2.0'},
+        {'name': 'passlib', 'version': '3.0.0'},
+    ]
+
+
+@app.route('/')
+def index():
+    test_packages = get_latest_packages()
+    return flask.render_template('home/index.html', packages=test_packages)
+
+
+@app.route('/about')
+def about():
+    return flask.render_template('home/about.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
