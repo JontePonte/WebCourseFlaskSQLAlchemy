6836e5fc06842dca907d0a87b0eb929b7befd042
JontePonte <john_nordstrand@hotmail.com>
2020-11-23 23:03:56 +0100

    first use of db query implemented

--- 07SQLAlchemyUsage/pypi_org/services/package_service.py
+++ 07SQLAlchemyUsage/pypi_org/services/package_service.py
@@ -1,8 +1,35 @@
 
-def get_latest_packages():
-    return [
-        {'name': 'flask', 'version': '1.2.3'},
-        {'name': 'sqlalchemy', 'version': '2.2.0'},
-        {'name': 'passlib', 'version': '3.0.0'},
-        {'name': 'John!', 'version': '3.0.0'},
-        ]
+import pypi_org.data.db_session as db_session
+import sqlalchemy.orm
+
+from typing import List
+
+from pypi_org.data.package import Package
+from pypi_org.data.releases import Release
+
+
+def get_latest_releases(limit = 10) -> List[Release]:
+    session = db_session.create_session()
+
+    # options make the system oly call db once
+    # limit limit to 10 values
+    # all create list
+    releases = session.query(Release).\
+        options(sqlalchemy.orm.joinedload(Release.package)).\
+        order_by(Release.created_date.desc()).\
+        limit(limit).\
+        all()
+
+    session.close()
+
+    return releases
+
+
+def get_package_count() -> int:
+    session = db_session.create_session()
+    return session.query(Package).count()
+
+
+def get_release_count():
+    session = db_session.create_session()
+    return session.query(Release).count()
--- 07SQLAlchemyUsage/pypi_org/views/home_views.py
+++ 07SQLAlchemyUsage/pypi_org/views/home_views.py
@@ -1,16 +1,22 @@
 import flask
 
 import pypi_org.services.package_service as package_service
+import pypi_org.services.user_service as user_service
 
-blueprint = flask.Blueprint('home', __name__, template_folder='templates')
+blueprint = flask.Blueprint("home", __name__, template_folder="templates")
 
 
-@blueprint.route('/')
+@blueprint.route("/")
 def index():
-    test_packages = package_service.get_latest_packages()
-    return flask.render_template('home/index.html', packages=test_packages)
+    return flask.render_template(
+        "home/index.html",
+        releases=package_service.get_latest_releases(),
+        package_count=package_service.get_package_count(),
+        release_count=package_service.get_release_count(),
+        user_count=user_service.get_user_count(),
+    )
 
 
-@blueprint.route('/about')
+@blueprint.route("/about")
 def about():
-    return flask.render_template('home/about.html')
+    return flask.render_template("home/about.html")
