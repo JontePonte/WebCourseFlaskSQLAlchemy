84a464b312e53dfba1584b243a16b1c7d55377aa
JontePonte <john_nordstrand@hotmail.com>
2020-11-30 07:42:10 +0100

    users can be saved to database and login

--- 09UserInput/pypi_org/app.py
+++ 09UserInput/pypi_org/app.py
@@ -26,10 +26,12 @@ def register_blueprints():
     from pypi_org.views import home_views
     from pypi_org.views import package_views
     from pypi_org.views import cms_views
+    from pypi_org.views import account_views
 
     app.register_blueprint(home_views.blueprint)
     app.register_blueprint(package_views.blueprint)
     app.register_blueprint(cms_views.blueprint)
+    app.register_blueprint(account_views.blueprint)
 
 
 if __name__ == "__main__":
--- 09UserInput/pypi_org/views/account_views.py
+++ 09UserInput/pypi_org/views/account_views.py
@@ -1,29 +1,86 @@
 import flask
 
-blueprints = flask.Blueprint('account', __name__, template_folder="templates")
+import pypi_org.services.user_service as user_service
+
+blueprint = flask.Blueprint('account', __name__, template_folder="templates")
 
 
 """ --------------- Account --------------- """
-@blueprints.route('/account')
+
+
+@blueprint.route('/account')
 def index():
     return flask.render_template('account/index.html')
 
 
 """ --------------- Register --------------- """
-@blueprints.route('/account/register', methods=['GET'])
+
+
+@blueprint.route('/account/register', methods=['GET'])
 def register_get():
     return flask.render_template('account/register.html')
 
-@blueprints.route('/account/register', methods=['POST'])
+
+@blueprint.route('/account/register', methods=['POST'])
 def register_post():
-    return flask.render_template('account/register.html')
+    r = flask.request
+
+    name = r.form.get('name', '')
+    email = r.form.get('email', '').lower().strip()
+    password = r.form.get('password', '').strip()
+
+    if not name or not email or not password:
+        return flask.render_template(
+            'account/register.html',
+            name=name,
+            email=email,
+            password=password,
+            error="Some required fields are missing.",
+            )
+    # TOTO: Create the user
+    user = user_service.create_user(name, email, password)
+    if not user:
+        return flask.render_template(
+            'account/register.html',
+            name=name,
+            email=email,
+            password=password,
+            error="A user with that email already exists.",
+            )
+    # Log in browser as a session
+    return flask.redirect('/account')
 
 
 """ --------------- Login --------------- """
-@blueprints.route('/account/login', methods=['GET'])
+
+
+@blueprint.route('/account/login', methods=['GET'])
 def login_get():
     return flask.render_template('account/login.html')
 
-@blueprints.route('/account/login', methods=['POST'])
+
+@blueprint.route('/account/login', methods=['POST'])
 def login_post():
-    return flask.render_template('accound/login.html')
+    r = flask.request
+
+    email = r.form.get('email', '').lower().strip()
+    password = r.form.get('password', '').strip()
+
+    if not email or not password:
+        return flask.render_template(
+            'account/login.html',
+            email=email,
+            password=password,
+            error="Please enter email and password.",
+            )
+
+    user = user_service.login_user(email, password)
+    if not user:
+        return flask.render_template(
+            'account/login.html',
+            email=email,
+            password=password,
+            error="The account does not exist or the password is wrong.",
+            )
+    # Log in browser as a session
+    return flask.redirect('/account')
